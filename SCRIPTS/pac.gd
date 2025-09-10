02520f3fbafb0225e69c17eee07e6cd75980cd22
Oktay Sırmaçekici <79636703+Selulox@users.noreply.github.com>
2025-09-10 13:07:19 +0300

    player

--- SCRIPTS/pac.gd
+++ SCRIPTS/pac.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 300
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var player_health = 3
+
+const SPEED = 300
+const damage_rate = 1
+signal health_depleted
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit_pause"):
@@ -16,5 +20,14 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("down")
 	if Input.is_action_just_pressed("up"):
 		animated_sprite_2d.play("up")
-		
+	var input = Input.get_vector('left', 'right', 'up', 'down')
+	if input.length() > 0:
+		velocity = input * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	move_and_slide()
+
+func take_damage():
+	player_health -= 1
+	if player_health == 0:
+		queue_free()
